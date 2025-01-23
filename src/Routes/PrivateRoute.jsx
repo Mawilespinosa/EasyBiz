4114075009cd994310eb3ee1d6fa40dfccb4ca42
mawilespinosa <mawilespinosa@gmail.com>
2025-01-23 00:20:23 -0500

    se reparo los navigate en el navbar

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -5,8 +5,7 @@ import { useAuth } from "../contexts/AuthContext";
 const PrivateRoute = ({ allowedRoles }) => {
   const { user } = useAuth();
   return user && allowedRoles.includes(user.role) ? (
-    <Outlet />
-    
+    <Outlet />    
   ) : (
     <Navigate to="/EasyBiz/login" replace />
   );
